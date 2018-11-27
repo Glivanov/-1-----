c0c265360ca42cbb638f3056da3f87c3d877115f
Glivanov <45406062+Glivanov@users.noreply.github.com>
2018-11-28 00:53:59 +0300

    2.AnalyzeTriangle

---
+++ 1.ConeSquare.pas
@@ -0,0 +1,13 @@
+program Conesquare;
+var
+S,r,h,l: real;
+begin
+  write(‘r=');
+  readln(r);
+  write(‘h=');
+  readln(h);
+  l:=sqrt(r^2+l^2);
+  S:=pi*r^2+pi*r*l;
+  Writeln(S);
+  readln;
+end.
